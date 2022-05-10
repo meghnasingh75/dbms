41cff1a5a96871dad54cc74f3f0ef0b2a7a948a6
Meghna Singh <90127631+meghnasingh75@users.noreply.github.com>
2022-05-10 23:44:27 +0530

    Update Experiment-9.sql

--- codes/Experiment-9.sql
+++ codes/Experiment-9.sql
@@ -6,14 +6,12 @@ CREATE TABLE customers(
     salary number(10)
 );
 
-INSERT ALL
-INTO customers VALUES (1, 'Ramesh', 23, 'Allahbad', 20000)
-INTO customers VALUES (2, 'Suresh', 22, 'Kanpur', 22000)
-INTO customers VALUES (3, 'Mahesh', 24, 'Ghaziabad', 24000)
-INTO customers VALUES (4, 'Chandan', 25, 'Noida', 26000)
-INTO customers VALUES (5, 'Alex', 21, 'Paris', 28000)
-INTO customers VALUES (6, 'Sunita', 20, 'Delhi', 30000)
-SELECT 1 FROM DUAL;
+INSERT INTO customers VALUES (1, 'Ramesh', 23, 'Allahbad', 20000);
+INSERT INTO customers VALUES (2, 'Suresh', 22, 'Kanpur', 22000);
+INSERT INTO customers VALUES (3, 'Mahesh', 24, 'Ghaziabad', 24000);
+INSERT INTO customers VALUES (4, 'Chandan', 25, 'Noida', 26000);
+INSERT INTO customers VALUES (5, 'Alex', 21, 'Paris', 28000);
+INSERT INTO customers VALUES (6, 'Sunita', 20, 'Delhi', 30000);
 
 SET serveroutput on;
 
@@ -26,5 +24,6 @@ if sql%notfound then
 dbms_output.put_line(total_rows || ' customers updated ');
 end if;
 end;
+/
 
 SELECT * FROM customers;
