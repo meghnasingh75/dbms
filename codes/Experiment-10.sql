dc18aabb5a6e0a8a68627ba0cfc1a56ae19e432f
Meghna Singh <90127631+meghnasingh75@users.noreply.github.com>
2022-05-10 23:40:02 +0530

    Update Experiment-10.sql

--- codes/Experiment-10.sql
+++ codes/Experiment-10.sql
@@ -6,16 +6,13 @@ CREATE TABLE customers(
     salary number(10)
 );
 
-INSERT ALL
-INTO customers VALUES (1, 'Ramesh', 23, 'Allahbad', 20000)
-INTO customers VALUES (2, 'Suresh', 22, 'Kanpur', 22000)
-INTO customers VALUES (3, 'Mahesh', 24, 'Ghaziabad', 24000)
-INTO customers VALUES (4, 'Chandan', 25, 'Noida', 26000)
-INTO customers VALUES (5, 'Alex', 21, 'Paris', 28000)
-INTO customers VALUES (6, 'Sunita', 20, 'Delhi', 30000)
-SELECT 1 FROM DUAL;
 
-set linesize 300;
+INSERT INTO customers VALUES (1, 'Ramesh', 23, 'Allahbad', 20000);
+INSERT INTO customers VALUES (2, 'Suresh', 22, 'Kanpur', 22000);
+INSERT INTO customers VALUES (3, 'Mahesh', 24, 'Ghaziabad', 24000);
+INSERT INTO customers VALUES (4, 'Chandan', 25, 'Noida', 26000);
+INSERT INTO customers VALUES (5, 'Alex', 21, 'Paris', 28000);
+INSERT INTO customers VALUES (6, 'Sunita', 20, 'Delhi', 30000);
 
 SELECT * FROM customers;
 
@@ -26,11 +23,12 @@ when (new.id > 0)
 declare
 sal_diff number;
 begin
-sal_diff:= new.salary - old.salary;
-dbms_output.put_line('Old Salary:'||old.salary);
-dbms_output.put_line('New Salary:'||new.salary);
-dbms_output.put_line('Salary Difference:'||sal_diff);
+sal_diff:= :new.salary - :old.salary;
+dbms_output.put_line('Old Salary:'|| :old.salary);
+dbms_output.put_line('New Salary:'|| :new.salary);
+dbms_output.put_line('Salary Difference:'|| sal_diff);
 end;
+/
 
 set serveroutput on;
 
@@ -40,9 +38,10 @@ begin
 update customers
 set salary = salary + 5000;
 if sql%notfound then
-dbms_output.put_line('No customers updated')
+dbms_output.put_line('No customers updated');
 elsif sql%found then
 total_rows:= sql%rowcount;
 dbms_output.put_line(total_rows || ' customers updated');
 end if;
 end;
+/
