1cdd080c0cfa4d47a0e2ffd6ad48a42953013e8c
Meghna Singh <90127631+meghnasingh75@users.noreply.github.com>
2022-05-10 21:07:56 +0530

    Create Experiment-8.sql

---
+++ codes/Experiment-8.sql
@@ -0,0 +1,16 @@
+-- CREATING FUNCTION
+CREATE FUNCTION adder(n1 in number, n2 in number)
+RETURN number
+IS
+n3 number(8);
+BEGIN
+n3:=n1+n2;
+RETURN n3;
+END;
+
+-- CALLING FUNCTION
+DECLARE
+n3 number(2);
+n3:= adder(11, 22);
+dbms_output.put_line('Addition is: ' || n3);
+END;
